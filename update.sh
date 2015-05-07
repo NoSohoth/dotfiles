c5dcdad4f42450aefe78f8bd9c09deda6777bb7b
NoSohoth <NoSohoth@users.noreply.github.com>
2015-05-08 00:57:56 +0200

    Create a script to ease the dotfiles update process

---
+++ update.sh
@@ -0,0 +1,7 @@
+cp -r ~/.asoundrc ~/.i3 ~/.xinitrc ~/.Xresources ~/.ycm_extra_conf.py ~/.zshrc ~/Git/dotfiles/
+cp ~/.vim/vimrc ~/Git/dotfiles/.vimrc
+cp ~/.ncmpcpp/config ~/Git/dotfiles/.ncmpcpp/
+cp ~/.config/redshift.conf ~/Git/dotfiles/.config
+cp ~/.config/i3status/config ~/Git/dotfiles/.config/i3status
+cp ~/.config/mpd/mpd.conf ~/Git/dotfiles/.config/mpd
+sudo cp -r /etc/iptables/iptables.rules /etc/mplayer/mplayer.conf ~/Git/dotfiles/
