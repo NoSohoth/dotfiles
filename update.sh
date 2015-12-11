8e97bac65940233077c6609b8bfb47d1a0592541
NoSohoth <NoSohoth@users.noreply.github.com>
2015-12-11 19:06:27 +0100

    Switch from mplayer to mpv

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
-cp -r ~/.asoundrc ~/.i3 ~/.mozilla/firefox/wl0ducon.default/chrome/userChrome.css ~/.xinitrc ~/.Xresources ~/.ycm_extra_conf.py ~/.zshrc ~/Git/dotfiles/
+cp -r ~/.asoundrc ~/.i3 ~/.mozilla/firefox/j25xp3mj.default/chrome/userChrome.css ~/.xinitrc ~/.Xresources ~/.ycm_extra_conf.py ~/.zshrc ~/Git/dotfiles/
 cp ~/.vim/vimrc ~/Git/dotfiles/.vimrc
 cp ~/.ncmpcpp/config ~/Git/dotfiles/.ncmpcpp/
 cp ~/.config/redshift.conf ~/Git/dotfiles/.config
 cp ~/.config/mpd/mpd.conf ~/Git/dotfiles/.config/mpd
-sudo cp /etc/iptables/iptables.rules /etc/mplayer/mplayer.conf ~/Git/dotfiles/
+sudo cp /etc/iptables/iptables.rules /etc/mpv/mpv.conf ~/Git/dotfiles/
